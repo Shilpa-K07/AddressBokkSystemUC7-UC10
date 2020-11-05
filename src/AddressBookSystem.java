33f04f9e143bbad6b5513b0f53ffdee76d4a1b0a
shilpa <shilpa07udupi@gmail.com>
2020-11-05 10:09:25 +0530

    feat: add multiple address book

--- src/AddressBookSystem.java
+++ src/AddressBookSystem.java
@@ -1,8 +1,40 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class AddressBookSystem {
 	public static void main(String[] args) {
-		System.out.println("Welcome to AddressBook System");
+		Scanner scanner = new Scanner(System.in);
 		
-		AddressBookDao addressBook = new AddressBookDao();
-		addressBook.getUserChoice();
+		System.out.println("Welcome to AddressBook System");
+		HashMap<String, AddressBookDao> addressBookList = new HashMap<String, AddressBookDao>();
+		
+		AddressBookDao familyAddressBook = new AddressBookDao();
+        AddressBookDao officeAddressBook = new AddressBookDao();
+        addressBookList.put("Family", familyAddressBook);
+        addressBookList.put("Ofiice", officeAddressBook);
+        
+        boolean isTerminate = false;
+        
+        while(!isTerminate) {
+        	System.out.println("Choose addres book");
+        	System.out.println("1: Family address book \n" +
+        			"2: Office address book \n"+
+        			"3: Exit");
+        	int option = scanner.nextInt();
+        	switch(option) {
+        	case 1:
+        		familyAddressBook.getUserChoice();
+        		break;
+        	case 2:
+        		officeAddressBook.getUserChoice();
+        		break;
+        	case 3:
+        		isTerminate = true;
+        		break;
+        	default:
+        		System.out.println("Please enter correct option");
+        		break;
+        	}
+        }
 	}
 }
