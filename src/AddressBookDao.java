37b6ce95e4bba9bb2063edaf5327e9ccddf3f10c
shilpa <shilpa07udupi@gmail.com>
2020-11-05 09:40:16 +0530

    feat: update contact in address book

--- src/AddressBookDao.java
+++ src/AddressBookDao.java
@@ -6,9 +6,13 @@ public class AddressBookDao {
 	Scanner scanner = new Scanner(System.in);
 	
 	public void getUserChoice() {
+		boolean isTerminate = false;
+		while (!isTerminate) {
 		System.out.println("Enter your option");
 		System.out.println("1: Add new contact \n" +
-				"2: Exit \n");
+				"2: Update contact \n" +
+				"3: Print address book \n"+
+				"4: Exit \n");
 		int option = scanner.nextInt();
 		switch(option) {
 			case 1:
@@ -17,14 +21,91 @@ public class AddressBookDao {
 		 		System.out.println("Contact added !");
 		 		break;
 			case 2:
-				System.out.println("Exiting..");
+				updateContact();
                 break;
+			case 3:
+				System.out.println(contactList);
+				break;
+			case 4:
+				isTerminate = true;
+				break;
 			default:
 				System.out.println("Please provide correct option");
 				break;
+			}
 		}
 	}
 	
+	private void updateContact() {
+		System.out.println("Enter emailId to update");
+		String emailId = scanner.next();
+
+		if (!contactList.containsKey(emailId)) {
+			System.out.println("Email Id provided is wrong !");
+			updateContact();
+        }
+		
+		PersonEntity person = contactList.get(emailId);
+		boolean isTerminate = false;
+		while(!isTerminate) {
+			System.out.println("Which field do you want to update?");
+			System.out.println("1: First name \n" +
+	                "2: Last Name \n" +
+	                "3: Address \n" +
+	                "4: City \n" +
+	                "5: State \n" +
+	                "6: Zip \n" +
+	                "7: PhoneNumber \n" +
+	                "8: Exit \n");
+
+	        int option = scanner.nextInt();
+	        switch (option) {
+	            case 1:
+	                System.out.println("Enter First Name ");
+	                String firstName = scanner.next();
+	                person.setFirstName(firstName);
+	                break;
+	            case 2:
+	                System.out.println("Enter Last Name");
+	                String lastName = scanner.next();
+	                person.setLastName(lastName);
+	                break;
+	            case 3:
+	                System.out.println("Enter Address");
+	                String address = scanner.next();
+	                person.setAddress(address);
+	                break;
+	            case 4:
+	                System.out.println("Enter City");
+	                String city = scanner.next();
+	                person.setCity(city);
+	                break;
+	            case 5:
+	                System.out.println("Enter State");
+	                String state = scanner.next();
+	                person.setState(state);
+	                break;
+	            case 6:
+	                System.out.println("Enter Zip Code");
+	                int zip=scanner.nextInt();
+	                person.setZip(zip);
+	                break;
+	            case 7:
+	                System.out.println("Enter Phone Number");
+	                long phone = scanner.nextLong();
+	                person.setPhoneNumber(phone);
+	                break;
+	            case 8:
+	            	System.out.println("Exiting..");
+	            	isTerminate = true;
+	            default:
+	                System.out.println("please select valid option");
+	                break;
+	        }
+	        System.out.println("Details updated!\n");
+		}
+	}
+
 	private PersonEntity getUserInput() {
 		System.out.println("Enter First Name");
 		String firstName = scanner.next();
